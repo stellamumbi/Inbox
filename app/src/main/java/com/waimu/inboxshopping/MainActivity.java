10d550ab397bc5f0b12108b3319ac6627f61416c
stellamumbi <mumbistella5@gmail.com>
2020-03-03 17:17:43 +0300

    first commit

---
+++ app/src/main/java/com/waimu/inboxshopping/MainActivity.java
@@ -0,0 +1,26 @@
+package com.waimu.inboxshopping;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int SPLASH_TIME_OUT = 4000;
+        boolean b = new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
